7a1a9d77a0082d50da5c1a5ebaa222dbf035581b
Adam Dziedzic <adam.cajf@gmail.com>
2021-11-05 09:36:27 -0400

    get models cpu

--- models/get_models.py
+++ models/get_models.py
@@ -3,10 +3,14 @@ import torch
 import os
 
 
-def get_model(dataset: str, checkpoint_dir: str, device):
+def get_model(dataset: str, checkpoint_dir: str, device: str):
+    print('device: ', device)
+
     model_file = os.path.join(checkpoint_dir, dataset, dataset + '.pt')
     if dataset == 'mnist':
-        model = MnistNet().to(device)
-        model.load_state_dict(torch.load(model_file))
+        model = MnistNet()
+        if device == 'cuda' or device == torch.device('cuda'):
+            model = model.to(device)
+        model.load_state_dict(torch.load(model_file, map_location=torch.device(device)))
 
     return model
